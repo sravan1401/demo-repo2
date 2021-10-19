ebfa6dfe432e2c124d93ab9d3cb8c4ca1cd96340
Sravan Jeeru <sravanjeeru@gmail.com>
2021-10-19 14:17:29 -0400

    For Jenkins Use

---
+++ Untitled-1.py
@@ -0,0 +1,8 @@
+hero_name = "Sravan"
+herione_name = "Neha"
+int (hero_age) = 27
+print( "This is the story of a couple named " + hero_name + " and " + herione_name + " who live in Canada at age " + hero_age + ".")
+
+hero_name = "Sam"
+herione_name ="Suzi"
+print( "This is the story of a couple named " + hero_name + " and " + herione_name + " who live in Canada at age " + hero_age + ".")
